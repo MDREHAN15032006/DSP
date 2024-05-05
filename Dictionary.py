6560075f207917fabf1470cfd578d51fdb8a5f48
MDREHAN15032006 <143314290+MDREHAN15032006@users.noreply.github.com>
2024-05-05 17:09:34 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+
+
+Dict={}
+print("Empty dictionary:")
+print(Dict)
+Dict=dict({1:'Mon',2:'Tue',3:'Wed'})
+print("Dictionary with use of dict():")
+print(Dict)
+Dict=dict([(1,'January'),(2,'February')])
+print("Dictionary with each item as a pair:")
+print(Dict)
